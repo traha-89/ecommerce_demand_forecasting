d75686b5c143dea3e8186918798f4bdb324952c2
traha-89 <167479295+traha-89@users.noreply.github.com>
2025-11-12 23:37:17 +0100

    Create 03_exploratory_analysis.sql

---
+++ sql/03_exploratory_analysis.sql
@@ -0,0 +1,15 @@
+-- -----------------------EXPLORATORY DATA ANALYSIS-------------------------------------
+-- Calculate Summary Stats (Mean, Median, std dev, min, max) for key metrics
+
+
+
+
+-- Analyze patterns by day of week, weekend/weekday, holiday/non-holiday, promo/non-promo
+
+
+
+-- Calculate monthly and quarterly aggregations
+
+
+
+-- Idenitfy seasonal trends
